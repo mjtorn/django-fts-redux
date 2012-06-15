e0aa665a26620af56de87c32f8956c982bc764a4
Markus Törnqvist <mjt+illyria@skyhood.com>
2012-06-15 16:47:57 +0300

    Remove db backend check that makes no sense
    Maybe it did before the age of multidb and dbrouters

--- fts/backends/pgsql.py
+++ fts/backends/pgsql.py
@@ -40,11 +40,6 @@ class VectorField(models.Field):
 
 class SearchClass(BaseClass):
     def __init__(self, server, params):
-        from django.conf import settings
-        
-        if not settings.DATABASE_ENGINE in ['postgresql', 'postgresql_psycopg2']:
-            raise InvalidFtsBackendError("PostgreSQL with tsearch2 support is needed to use the pgsql FTS backend")
-        
         self.backend = 'pgsql'
 
 class SearchManager(BaseManager):
